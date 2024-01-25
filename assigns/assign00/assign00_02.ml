a03376d6b9e055eed7070fede28e0a39089a091e
uenishia26 <uenishia@bu.edu>
2024-01-25 19:27:09 +0900

    Works for all test cases

--- assigns/assign00/assign00_02.ml
+++ assigns/assign00/assign00_02.ml
@@ -17,5 +17,18 @@
 
 *)
 
-let is_prime (n : int) : bool =
-  (* YOUR CODE GOES HERE *)
+let rec is_prime (n : int) : bool =
+   if n = 1 || n = 0
+      then false 
+   else if n = 2 (*Three base canse for when n = 0,1,2*)
+      then true
+   else
+      let rec loop i = (*Way to create a recursive loop*)
+         if i >= n then (*regular loop but using a recursive function *)
+            true
+         else if(n mod i) = 0 then (*This is the defnition of prime number. If any number exists divisible from 2 to n-1 then its false*)
+            false
+         else 
+            loop(i+1) 
+         in loop 2
+
