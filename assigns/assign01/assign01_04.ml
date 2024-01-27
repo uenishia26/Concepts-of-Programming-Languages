8d755efd8d47ed73730fb76baa9c6bded829e31f
uenishia26 <uenishia@bu.edu>
2024-01-27 18:51:48 +0900

    With comments. Completed taxicab

--- assigns/assign01/assign01_04.ml
+++ assigns/assign01/assign01_04.ml
@@ -16,21 +16,26 @@
 
  *)
 
+ (*Logic description: 
+    Obviously adding to the upperbound will get a value greater than n thus we use this fact
+    as the main way to control the loops. Basically what happens is we check every combination of a and b 
+    and keep track of the counter everytime. *)
+    
 let taxicab (n : int) : int =
-  if n = 2 then 1
+  if n = 2 then 1 (*Base case*)
   else 
-    let rec loop a b counter: int = 
-      let upperBound = int_of_float(float_of_int(n) ** (1.0 /. 3.0)) in 
-      if a = upperBound (*When a reaches the upperBound terminate loop *)
+    let rec loop a b counter: int = (*a,b, and variation counter*)
+      let upperBound = int_of_float(float_of_int(n) ** (1.0 /. 3.0)) in (*Getting the upperBound by finding cube root of n*)
+      if a = upperBound (*When a reaches the upperBound terminate loop *) 
         then 
-          counter
-      else
+          counter 
+      else 
         if b = upperBound then loop (a+1) 1 counter (*Reset b with inc a*)
         else 
-          if (a * a * a) + (b * b * b) = n then
-            loop a (b+1) (counter+1)
-          else loop a (b+1) (counter)
-    in loop 1 1 0;;
+          if (a * a * a) + (b * b * b) = n then (*Check the cube of a & b and if eqaul to n addition*)
+            loop a (b+1) (counter+1) (*Incrmenet both counter and b val*)
+          else loop a (b+1) (counter) (*Just inc b*)
+    in loop 1 1 0;; (*Call loop with values 1 1 0*)
 
 
 
