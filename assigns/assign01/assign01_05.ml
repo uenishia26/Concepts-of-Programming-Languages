c94109e59dd72266a3d70d6730ea0f8cc5558320
uenishia26 <uenishia@bu.edu>
2024-01-28 18:38:28 +0900

    At least first part works but now about to adjust so need to save

--- assigns/assign01/assign01_05.ml
+++ assigns/assign01/assign01_05.ml
@@ -38,4 +38,28 @@
  *)
 
 let block_text (s : string) (min_width : int) (max_width : int) : string =
-  assert false (* REMOVE THIS LINE AND FILL IN YOUR SOLUTION *)
+    (*If divisible by max_width, we don't care about the min_width requirment *)
+  if (String.length s) mod (max_width) = 0 then
+    let rec subStringloop x y counter currentStr : string = (*Parameters here are start position for substring, max_width counter and newStr*)
+      if counter = (String.length s) / max_width then (*Base case we return the string*)
+        currentStr
+      else (*Two cases for concatenation. Looking out for last case so we don't add \n in the last line vs all the other cases*)
+        if (counter+1) = (String.length s) / max_width then 
+          subStringloop (x+y) y (counter+1) (currentStr ^ (String.sub s x y)) (*We don't add \n to the last line *)
+        else subStringloop (x+y) y (counter+1) (currentStr ^ (String.sub s x y) ^ "\n") (*We add \n to the last line *)
+    in subStringloop 0 max_width 0 ""
+
+  (*When string not divisble by max_width we have two case
+     1) remainder after mod with max > min width
+     2) remainder after mod with max < min width*)
+  else
+    if (String.length s) mod (max_width) > min_width then
+      let rec subStringlooptwo x y counter current:Str : String = 
+        
+        
+      in subStringlooptwo 
+
+
+
+      
+
